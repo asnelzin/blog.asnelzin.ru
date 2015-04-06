fafcd07f4c05677d74effeaf9bb23153e209163d
Alexander Nelzin <a.nelzin@netrika.ru>
2015-04-06 11:50:52 +0300

    [wip] Add initial functionality

--- blog/apps/content/models.py
+++ blog/apps/content/models.py
@@ -7,12 +7,11 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class Post(models.Model):
     title = models.CharField('Заголовок', max_length=700)
     body = models.TextField('Контент', blank=True)
     slug = models.SlugField('Слаг', max_length=70, help_text='URL', unique=True)
-    date = models.DateField('Дата публикации', default=date.now())
+    date = models.DateField('Дата публикации', default=date.today())
 
     class Meta:
         verbose_name = 'пост'
