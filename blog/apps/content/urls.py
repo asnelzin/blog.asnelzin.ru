2cf0fb06a1373959d18b57f0a2ef64421c8dc1e7
Alexander Nelzin <asnelzin@gmail.com>
2015-04-11 14:00:22 +0300

    [wip] Add posts-with-tag page

--- blog/apps/content/urls.py
+++ blog/apps/content/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from blog.apps.content.views import PostList, PostDetail
+from blog.apps.content.views import PostList, PostDetail, PostListTag
 
 urlpatterns = patterns(
     '',
     url(r'^$', PostList.as_view(), name='main'),
-    url(r'^(?P<slug>[-_\w]+)/$', PostDetail.as_view(), name='post_view')
+    url(r'^(?P<slug>[-_\w]+)/$', PostDetail.as_view(), name='post_view'),
+    url(r'^tags/(?P<slug>[-_\w]+)/$', PostListTag.as_view(), name='post_list_tag')
 )
--- blog/apps/content/views.py
+++ blog/apps/content/views.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
+from django.shortcuts import get_object_or_404
 
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from taggit.models import Tag
 from blog.apps.content.models import Post
 
 
@@ -13,3 +15,17 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
     template_name = 'content/post_detail.html'
+
+
+class PostListTag(ListView):
+    model = Post
+    template_name = 'content/post_list_tag.html'
+
+    def get_queryset(self):
+        self.tag = get_object_or_404(Tag, slug__iexact=self.kwargs['slug'])
+        return Post.objects.filter(tags__in=[self.tag])
+
+    def get_context_data(self, **kwargs):
+        context = super(PostListTag, self).get_context_data(**kwargs)
+        context['tag'] = self.tag
+        return context
