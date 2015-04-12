72587ffcc89b448ce54025cee4910160a21b3998
Alexander Nelzin <asnelzin@gmail.com>
2015-04-12 23:35:33 +0300

    Add all posts page

--- blog/apps/content/urls.py
+++ blog/apps/content/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
-from blog.apps.content.views import PostList, PostDetail, PostListTag
+from blog.apps.content.views import PostList, PostDetail, PostListTag, AllPostList
 
 urlpatterns = patterns(
     '',
     url(r'^$', PostList.as_view(), name='main'),
+    url(r'^all/$', AllPostList.as_view(), name='all_post_view'),
     url(r'^(?P<slug>[-_\w]+)/$', PostDetail.as_view(), name='post_view'),
-    url(r'^tags/(?P<slug>[-_\w]+)/$', PostListTag.as_view(), name='post_list_tag')
+    url(r'^tags/(?P<slug>[-_\w]+)/$', PostListTag.as_view(), name='post_list_tag'),
 )
--- blog/apps/content/views.py
+++ blog/apps/content/views.py
@@ -1,10 +1,12 @@
 from __future__ import unicode_literals
+from collections import OrderedDict
 from django.shortcuts import get_object_or_404
 
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from taggit.models import Tag
 from blog.apps.content.models import Post
+
 
 
 class PostList(ListView):
@@ -28,4 +30,17 @@ class PostListTag(ListView):
     def get_context_data(self, **kwargs):
         context = super(PostListTag, self).get_context_data(**kwargs)
         context['tag'] = self.tag
+        return context
+
+
+class AllPostList(ListView):
+    model = Post
+    template_name = 'content/all_post_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AllPostList, self).get_context_data(**kwargs)
+        object_list = OrderedDict()
+        for date in Post.objects.dates('date', 'year', order='DESC'):
+            object_list[date.year] = Post.objects.filter(date__year=date.year)
+        context['object_list'] = object_list
         return context
